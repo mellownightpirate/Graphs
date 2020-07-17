8ac2813e4e4dbe799743c11261bd28a7f00ccf3a
mellownightpirate <a@hasan.co>
2020-07-17 10:12:50 +0100

    adventure

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -29,7 +29,49 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+reverse_dirs = {'n': 's',
+                's': 'n',
+                'e': 'w',
+                'w': 'e'}
+reverse_path = []
+visited = {}
 
+def get_random_dir(directions):
+	i = random.randrange(0, len(directions))
+	return directions[i]
+
+# create adjacency list of current room and its exits
+visited[player.current_room.id] = player.current_room.get_exits()
+
+while len(visited) < len(room_graph) - 1:
+
+    if player.current_room.id not in visited:
+        # mark this room as visited and track its exits: use a list rather than a dict with ? for simplicity
+        exits = player.current_room.get_exits()
+        # shuffle our list of exits so we are not biased towards moving east
+        random.shuffle(exits)
+        visited[player.current_room.id] = exits
+
+        if reverse_path is not None:
+            last_room = reverse_path[-1]
+            # we don't want to revisit the room we have just come from, so remove it
+            visited[player.current_room.id].remove(last_room)
+
+    # if we have no more exits available, time to backtrack using our reverse path
+    while reverse_path is not None and len(visited[player.current_room.id]) < 1:
+        prev_path = reverse_path.pop()
+        traversal_path.append(prev_path)
+        player.travel(prev_path)
+
+    # travel to the next exit and keep track of our reverse path for backtracking
+    else:
+        exit = visited[player.current_room.id].pop()
+        # append the reverse direction to reversed path
+        reverse_path.append(reverse_dirs[exit])
+        # append the exit to the traversal path
+        traversal_path.append(exit)
+        # move to the next room
+        player.travel(exit)
 
 # TRAVERSAL TEST
 visited_rooms = set()
